643f51d0046c4e5a1f024a7353c36e1625888ba9
David Colón <david@davidiq.com>
2025-10-23 09:43:47 -0400

    feat: Allow environment configuration of card values

--- src/types/cardValues.ts
+++ src/types/cardValues.ts
@@ -1,18 +1,34 @@
-const cardValues = (): string[] => [
-  '0',
-  '0.5',
-  '1',
-  '2',
-  '3',
-  '5',
-  '8',
-  '13',
-  '20',
-  '40',
-  '100',
-  'infinite',
-  '?',
-  'coffee'
-]
+const cardValues = (): string[] => {
+  const defaultValues = [
+    '0',
+    '0.5',
+    '1',
+    '2',
+    '3',
+    '5',
+    '8',
+    '13',
+    '20',
+    '40',
+    '100',
+    'infinite',
+    '?',
+    'coffee'
+  ]
+
+  const environmentValues = (process.env.NEXT_PUBLIC_CARD_VALUES ?? '').split(
+    ','
+  )
+  if (environmentValues.length > 0) {
+    const filteredVerifiedValues = environmentValues.filter(value =>
+      defaultValues.includes(value)
+    )
+    return filteredVerifiedValues.length > 0
+      ? filteredVerifiedValues
+      : defaultValues
+  }
+
+  return defaultValues
+}
 
 export default cardValues
